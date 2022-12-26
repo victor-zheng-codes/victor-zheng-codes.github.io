29b17dd043e9415e39117ddaf323af95a39d4b69
Victor Zheng <zhengvictor0@gmail.com>
2022-12-26 12:56:30 -0800

    synchronize testing of buttons and js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
@@ -11,6 +12,26 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    // Active sections class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if (id){
+            // remove selected from the other btns
+            sectBtns.forEach((button) => {
+                button.classList.remove('active')
+            })
+            e.target.classList.add('active')
+        }
+
+        // hide other sections
+        sections.forEach((section)=>{
+            section.classList.remove('active')
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active')
+    })
 }
 
 PageTransitions();
